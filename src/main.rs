81091d8de35ecb886c1cf05f250e8af002e083b9
Paul Colin Hennig <8d_fvfmaa-o2wf_79aqig6g2ki6-09ffkeqmyo3d@y.gy>
2022-10-03 19:38:15 +0200

    use jemalloc

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+#[cfg(not(target_env = "msvc"))]
+use tikv_jemallocator::Jemalloc;
+
+#[cfg(not(target_env = "msvc"))]
+#[global_allocator]
+static GLOBAL: Jemalloc = Jemalloc;
+
 use actix_cors::Cors;
 use actix_web::{http, web, App, HttpServer};
 use anyhow::{bail, Context};
